8a0d469a6e7771331ffdae14d5a33e328a852619
Michael de S. Marcos <michael.smarcos@hotmail.com>
2018-08-15 10:00:17 -0300

    Inserida pasta para materiais da faculdade

--- HXTutors/sistema-login-com-niveis/sistema/app/controllers/CadastroController.php
+++ HXTutors/sistema-login-com-niveis/sistema/app/controllers/CadastroController.php
@@ -2,6 +2,10 @@
 
 class CadastroController extends \HXPHP\System\Controller {
 	
+	public function cadastrarAction(){
+		$this->view->setFile('index');
+	}
+	
 }
 
 ?>
---
+++ HXTutors/sistema-login-com-niveis/sistema/app/models/User.php
@@ -0,0 +1,6 @@
+<?php
+
+class User extends \HXPHP\System\Model
+{
+	
+}
